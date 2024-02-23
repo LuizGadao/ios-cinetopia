4b02e31d732356a1a56af5c65f4653f34cff1d41
Luiz <luizgadao@gmail.com>
2024-02-23 18:47:19 -0300

    create tableview

--- cinetopia/MoviesViewController.swift
+++ cinetopia/MoviesViewController.swift
@@ -7,7 +7,33 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource {
+    
+    private var names = [
+        "Luiz 🇧🇷",
+        "Carlos 😎",
+        "Luiz 🤪",
+        "Carlos 🥶"
+    ]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +41,21 @@ class MoviesViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .background
         setupNavigationBar()
+        addSubViews()
+        setupConstraints()
+    }
+    
+    private func addSubViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
     
     private func setupNavigationBar() {
@@ -31,16 +72,5 @@ class MoviesViewController: UIViewController {
         
         navigationItem.setHidesBackButton(true, animated: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
